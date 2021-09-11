ecc61ec5748824d10b110762ac6feb2c46ed9d4f
1301688 <waikiki.water.130@gmail.com>
2021-09-11 10:05:30 +0900

    first commit

---
+++ src/components/ArticleContent.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import { MDXProvider } from "@mdx-js/react"
+
+const components = {
+  pre: props => <div {...props} />,
+  code: props => (
+    <pre style={{ color: "aqua", backgroundColor: "black" }} {...props} />
+  ),
+}
+
+const ArticleContent = ({ post }) => {
+  return (
+    <MDXProvider components={components}>
+      <MDXRenderer>{post.body}</MDXRenderer>
+    </MDXProvider>
+  )
+}
+
+export default ArticleContent
---
+++ src/components/MDXcomponents/Highlight.js
@@ -0,0 +1,23 @@
+import React from "react"
+import styled from "styled-components"
+// import PrismHighlight, { defaultProps } from "prism-react-renderer"
+// import theme from "prism-react-renderer/themes/vsDark"
+
+const HighlightStyle = styled.div`
+  background-color: #2d2d2d;
+  font-family: monospace;
+  font-size: 12, 14;
+  overflow: auto;
+  padding: 5 0;
+  float: left;
+  min-width: 100%;
+`
+const Box = styled.div`
+  background-color: #2d2d2d;
+`
+
+const Highlight = () => {
+  return <Box></Box>
+}
+
+export default Highlight
